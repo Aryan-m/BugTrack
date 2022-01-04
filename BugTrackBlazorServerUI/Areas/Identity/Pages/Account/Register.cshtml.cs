86cd6798da94a1916af0e10ae91824e21e873bd9
Aryan-m <aryan_m96@yahoo.com>
2022-01-03 20:28:36 -0800

    register and logout done

--- BugTrackBlazorServerUI/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ BugTrackBlazorServerUI/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -80,26 +80,31 @@ namespace BugTrackBlazorServerUI.Areas.Identity.Pages.Account
                 {
                     _logger.LogInformation("User created a new account with password.");
 
-                    var code = await _userManager.GenerateEmailConfirmationTokenAsync(user);
-                    code = WebEncoders.Base64UrlEncode(Encoding.UTF8.GetBytes(code));
-                    var callbackUrl = Url.Page(
-                        "/",
-                        pageHandler: null,
-                        values: new { /*area = "Identity", userId = user.Id, code = code, returnUrl = returnUrl*/ },
-                        protocol: Request.Scheme);
+                    // Hook to add email confirmation if needed later
+
+                    //var code = await _userManager.GenerateEmailConfirmationTokenAsync(user);
+                    //code = WebEncoders.Base64UrlEncode(Encoding.UTF8.GetBytes(code));
+                    //var callbackUrl = Url.Page(
+                    //    "/",
+                    //    pageHandler: null,
+                    //    values: new { /*area = "Identity", userId = user.Id, code = code, returnUrl = returnUrl*/ },
+                    //    protocol: Request.Scheme);
 
                     //await _emailSender.SendEmailAsync(Input.Email, "Confirm your email",
                     //    $"Please confirm your account by <a href='{HtmlEncoder.Default.Encode(callbackUrl)}'>clicking here</a>.");
 
-                    if (_userManager.Options.SignIn.RequireConfirmedAccount)
-                    {
-                        return RedirectToPage("RegisterConfirmation", new { email = Input.Email, returnUrl = returnUrl });
-                    }
-                    else
-                    {
-                        await _signInManager.SignInAsync(user, isPersistent: false);
-                        return LocalRedirect(returnUrl);
-                    }
+                    //if (_userManager.Options.SignIn.RequireConfirmedAccount)
+                    //{
+                    //    return RedirectToPage("RegisterConfirmation", new { email = Input.Email, returnUrl = returnUrl });
+                    //}
+                    //else
+                    //{
+                    //    await _signInManager.SignInAsync(user, isPersistent: false);
+                    //    return Redirect(returnUrl);
+                    //}
+
+                    await _signInManager.SignInAsync(user, isPersistent: false);
+                    return Redirect(returnUrl);
                 }
                 foreach (var error in result.Errors)
                 {
